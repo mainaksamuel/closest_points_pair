4efa2cc96e01705db8a29209942f60126f2dfdbc
mainaksamuel <mainasamkaranja@gmail.com>
2021-07-30 05:37:00 +0300

    Added backend and api application tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
 
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
 # Create your tests here.
+
+from backend.utils import parse_request, get_closest_pair
+from backend.models import Point, ClosestPointsData
+
+
+class ApiTests(TestCase):
+    def test_single_point_from_single_point_input(self):
+        string_input = "(2,3)"
+        parsed_points = parse_request(string_input)
+        closest_pair = get_closest_pair(parsed_points)
+        expected = [Point(2, 3)]
+
+        self.assertEqual(len(closest_pair), len(expected))
+        first_calculated = f"{ closest_pair[0] }"
+        first_expected = f"{expected[0]}"
+        self.assertEqual(first_calculated, first_expected)
+
+    def test_ignores_non_points_input(self):
+        string_input = "(2 this is invalid input"
+        parsed_points = parse_request(string_input)
+        closest_pair = get_closest_pair(parsed_points)
+        expected = []
+        self.assertEqual(len(closest_pair), len(expected))
+
+
+class ApiRestTests(APITestCase):
+    def test_creation_of_closest_points_pair_data(self):
+        url = reverse("points")
+        data = {"submitted_points": "(2,3), (1,1), (5,4)"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ClosestPointsData.objects.count(), 1)
+        self.assertEqual(
+            ClosestPointsData.objects.get().submitted_points, "(2,3), (1,1), (5,4)"
+        )
+        self.assertEqual(ClosestPointsData.objects.get().closest_pair, "(2, 3), (1, 1)")
+
+    def test_returns_status_400_for_non_point_input(self):
+        url = reverse("points")
+        data = {"submitted_points": "no points submitted"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Point
+from .utils import parse_request, get_closest_pair
+
+
+class BackendTests(TestCase):
+    def test_parses_integer_points_correctly(self):
+        string_input = "(2,3), (1,1), (5, 4)"
+        parsed_points = parse_request(string_input)
+        expected = [
+            Point(2, 3),
+            Point(1, 1),
+            Point(5, 4),
+        ]
+        self.assertEqual(parsed_points.get_count(), len(expected))
+        first_parsed = f"{ parsed_points[0] }"
+        first_expected = f"{expected[0]}"
+        self.assertEqual(first_parsed, first_expected)
+
+        second_parsed = f"{ parsed_points[1] }"
+        second_expected = f"{expected[1]}"
+        self.assertEqual(second_parsed, second_expected)
+
+    def test_parses_negative_points_correctly(self):
+        string_input = "(2,-3), (-5, 4)"
+        parsed_points = parse_request(string_input)
+        expected = [Point(2, -3), Point(-5, 4)]
+
+        self.assertEqual(parsed_points.get_count(), len(expected))
+        first_parsed = f"{ parsed_points[0] }"
+        first_expected = f"{expected[0]}"
+        self.assertEqual(first_parsed, first_expected)
+
+    def test_parses_floating_points_correctly(self):
+        string_input = "(3.142,-32), (-5.7689, 4)"
+        parsed_points = parse_request(string_input)
+        expected = [Point(3.142, -32), Point(-5.7689, 4)]
+
+        self.assertEqual(parsed_points.get_count(), len(expected))
+        first_parsed = f"{ parsed_points[0] }"
+        first_expected = f"{expected[0]}"
+        self.assertEqual(first_parsed, first_expected)
+
+        second_parsed = f"{ parsed_points[1] }"
+        second_expected = f"{expected[1]}"
+        self.assertEqual(second_parsed, second_expected)
+
+    def test_returns_correct_closest_points_pair(self):
+        string_input = "(2,3), (1,1), (5, 4)"
+        parsed_points = parse_request(string_input)
+        closest_pair = get_closest_pair(parsed_points)
+        expected = [Point(2, 3), Point(1, 1)]
+
+        self.assertEqual(len(closest_pair), len(expected))
+        first_calculated = f"{ closest_pair[0] }"
+        first_expected = f"{expected[0]}"
+        self.assertEqual(first_calculated, first_expected)
+
+        second_calculated = f"{ closest_pair[1] }"
+        second_expected = f"{expected[1]}"
+        self.assertEqual(second_calculated, second_expected)
