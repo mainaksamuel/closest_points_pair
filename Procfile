1c0154cf5bab49881cd30e68b281e4d632bd0853
mainaksamuel <mainasamkaranja@gmail.com>
2021-07-30 15:17:00 +0300

    Initial Production configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn closest_points_pair.wsgi --log-file -
--- closest_points_pair/settings.py
+++ closest_points_pair/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)7_-@ln%p$gf4l*w*!m+^#iwomveqi+j4shv)cj4j&y_@ic2i^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["closest-points-pair.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
@@ -62,6 +63,7 @@ SPECTACULAR_SETTINGS = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -148,6 +150,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = "/static/"
 
